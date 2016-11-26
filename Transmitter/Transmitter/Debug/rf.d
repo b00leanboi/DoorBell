4e5a702e47d15cdeeb1d9ff48baddc027bd7551a
Adam Malek <malekadas@gmail.com>
2016-11-26 21:14:26 +0000

    Button to Wake Up Atmega Add, UART libraries delete
    Atmega no longer runs and generates interrupt when the button is
    pressed. Now, the button will give the Atmega power and it, by the PWR
    pin will keep the power until the ring message is transmitted.
    UART deleted since it's no longer needed.

--- Transmitter/Transmitter/Debug/rf.d
+++ Transmitter/Transmitter/Debug/rf.d
@@ -17,7 +17,7 @@ rf.d rf.o: .././rf.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- .././uart.h .././rf.h
+ .././rf.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -54,7 +54,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
-
-.././uart.h:
 
 .././rf.h:
--- Transmitter/Transmitter/main.c
+++ Transmitter/Transmitter/main.c
@@ -4,39 +4,18 @@
  * Created: 26.09.2016 18:35:50
  * Author : malek
  */ 
-#define F_CPU 16000000
+#define F_CPU 8000000
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#define LED0 PB0
-#define BTN0 PD3
+#define PWR	PB1 //If high the ATMEGA will be powered.
 #include "rf.h"
-#include "uart.h"
-#define BAUD 115200
-#define UBRR F_CPU/16/BAUD-1
 
 char data[] = "---RING---";
-volatile uint8_t btnCounter; //Eliminating bouncing
-volatile uint8_t btnTimer;
 int main(void)
 {
-	_delay_ms(100);
-
-	// --- LED --- //
-	DDRB = (1<<LED0);
-	PORTB = (1<<LED0);
-
-	// --- BUTTON --- //
-	DDRD &= ~(1<<BTN0);
-	PIND |= (1<<BTN0);
-
-	EIMSK |= (1<<INT1); //Interrupt enable on INT1
-
-	TIMSK0 |= (1<<TOIE1); //TIMER0 overflow interrupt enable
-
-	// --- TIMER --- //
-	TCCR1B = (1<<CS12); // clk/256
-	TIMSK1 = (1<<TOIE1); //Overflow interrupt enable
+	DDRB = (1<<PWR);
+	PORTB |= (1<<PWR);
 
 	// --- RF --- //
 	RF_Initialize();
@@ -50,33 +29,10 @@ int main(void)
 	RF_Transmit(0x0000);
 	RF_Transmit(0xCC77);
 
-	// --- UART --- //
-	UART_Initlialise(UBRR);
-
-    while (1) 
-    {
-		if(btnCounter)
-		{
-			TCCR0B &= ~((1<<CS00) | (1<<CS02));
-			btnCounter = 0;
-			btnTimer = 0;
-			RF_TxStart(data, 0);
-		}
-    }
-}
-ISR(TIMER1_OVF_vect)
-{
-	PORTB ^= (1<<LED0);
-}
-ISR(TIMER0_OVF_vect)
-{
-	btnCounter++;
-}
-ISR(INT1_vect)
-{
-	if(!btnTimer)
-	{
-		btnTimer = 1;
-		TCCR0B |= (1<<CS02) | (1<<CS00);
-	}
+	// --- TRANSMITTING RING MESSAGE --- //
+	RF_TxStart(data,0);
+	_delay_ms(50);
+	while(RF_status.Tx != 0);
+	_delay_ms(500);
+	PORTB &= ~(1<<PWR);
 }
